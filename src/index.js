075c660464dda80be8bd4a94ccc353ca391dbdd0
Pedro Borges <oi@pedroborg.es>
2019-08-12 10:41:49 -0300

    Add custom id and use "transitionend" event

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-function Tinybar() {
-  const el = document.createElement('div')
-  el.className = 'tinybar'
-  el.style = 'background:#e2e8f0;height:0;position:fixed;left:0;right:0;top:0;transition:height .3s ease-in;width:100%;z-index:9999'
+function Tinybar(customId) {
+  let el, bar, isMounted
 
-  const bar = document.createElement('div')
-  bar.style = 'background:#90cdf4;height:100%;transition:width .2s ease-out;width:0'
+  function mount() {
+    el = document.createElement('div')
+    if (customId) {
+      el.id = customId
+    } else {
+      el.style = 'background:#e2e8f0;height:2px;position:fixed;left:0;right:0;top:0;transition:height .3s ease-in;width:100%;z-index:9999'
+    }
+    el.addEventListener('transitionend', ({ target }) => {
+      if (target === el) {
+        isMounted = !document.body.removeChild(target)
+      }
+    })
 
-  el.appendChild(bar)
-  document.body.appendChild(el)
+    bar = document.createElement('div')
+    if (! customId) {
+      bar.style = 'background:#90cdf4;height:100%;transition:width .2s ease-out;width:0'
+    }
+
+    el.appendChild(bar)
+    isMounted = document.body.appendChild(el)
+  }
 
   return {
     go: function(number) {
-      el.style.height = '2px'
+      if (! isMounted) {
+        mount()
+      }
 
       if (number > 0) {
         bar.style.width = `${number}%`
@@ -20,7 +36,6 @@ function Tinybar() {
       if (number >= 100) {
         bar.style.width = `100%`
         el.style.height = 0
-        setTimeout(() => bar.style.width = 0, 400)
       }
     }
   }
